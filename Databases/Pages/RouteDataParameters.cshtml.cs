2e651affb5f64e7188803c5e8860ac264163e934
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-03-01 15:48:46 +0000

    UPDATE

---
+++ Databases/Pages/RouteDataParameters.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Databases.Pages
+{
+    public class RouteDataParametersModel : PageModel
+    {
+        //care is needed with the bindmethod
+
+        [BindProperty(SupportsGet = true)]
+            public string Message { get; set; }//Message has to be spelt like the parameter in cshtml page
+
+
+
+
+        // public string Mess { get; set; }
+        //public void OnGet(string message)
+        // {
+        // Mess = message;
+        //}
+    }
+}
